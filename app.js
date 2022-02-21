83487ebc90f6aa5a26b99d9dde89093dbfb3d211
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-02-21 17:41:39 +0000

    Update app.js

--- app.js
+++ app.js
@@ -7,16 +7,3 @@ app.use(express.static('static'))
 app.listen(port, () => {
   console.log(`Nayan Patel Digital Support Notes. (APP LISTENING)`)
 })
-
-// Initializing the router from express
-var router = express.Router();
-
-app.get('/api', function(req, res){
-   res.attachment('sitelogo.png');
-   console.log(res.get('Content-Disposition'));
-   res.end();
-});
-app.listen(port, function(err){
-   if (err) console.log(err);
-   console.log("Server listening on PORT", port);
-});
