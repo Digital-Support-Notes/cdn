6c3932467b52c99690c08c038a5327ddea8a324e
Nayan Patel <79650289+PatelN123@users.noreply.github.com>
2022-02-21 17:28:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.listen(port, () => {
   console.log(`Nayan Patel Digital Support Notes. (APP LISTENING)`)
 })
 
-res.attachment('static/sitelogo.svg')
-// Content-Disposition: attachment; filename="sitelogo.svg"
-// Content-Type: image/svg
+// Initializing the router from express
+var router = express.Router();
+
+app.get('/api', function(req, res){
+   res.attachment('sitelogo.png');
+   console.log(res.get('Content-Disposition'));
+   res.end();
+});
+app.listen(PORT, function(err){
+   if (err) console.log(err);
+   console.log("Server listening on PORT", PORT);
+});
